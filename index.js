3f2e0f435de6128a2a85dc5bbf1eaaa0ae7f6069
Kevin Neff <kvnneff@gmail.com>
2016-03-09 10:21:53 -0800

    Provide an index of available field types.

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  string: require('./string'),
+  number: require('./number'),
+  url: require('./url'),
+  geoJSON: require('./geoJSON'),
+  image: require('./image'),
+  list: require('./list')
+}
